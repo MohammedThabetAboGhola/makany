11d1fd46d41654fe3fc3f054a7444086051f6ad5
YayaBardweel <Yahyed3@gmail.com>
2023-06-01 20:56:30 +0200

    YEHYE

--- lib/screens/01_home_page/Profile.dart
+++ lib/screens/01_home_page/Profile.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
 import '../../generated/assets.dart';
 
 import '../../utils/00_style/00_app_color.dart';
@@ -28,10 +27,21 @@ class _ProfileState extends State<Profile> {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
-
-            Image.asset(Assets.Person),
-
-
+          Padding(
+            padding: const EdgeInsets.only(left: 250, top: 50),
+            child: Container(
+              height: 135,
+              width: 135,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage(Assets.Person),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.all(Radius.elliptical(100, 100.0)),
+                border: Border.all(width: 1.0, color: const Color(0xff707070)),
+              ),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.only(left: 180, top: 70),
             child: Text(
@@ -83,37 +93,34 @@ class _ProfileState extends State<Profile> {
             padding: const EdgeInsets.only(left: 100, top: 124),
             child: Icon(Icons.phone),
           ),
-
           Padding(
             padding: const EdgeInsets.only(top: 200),
             child: Divider(
               thickness: 2,
-              color: Colors.black,
+              color: Colors.grey,
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 310),
+            padding: const EdgeInsets.only(top: 250),
             child: Divider(
               thickness: 3,
               color: Colors.grey,
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 70, left: 200),
-            child: Center(
-              child: Text(
-                'آخر المشاهدات',
-                style: TextStyle(
-                  fontFamily: 'Tajawal',
-                  fontSize: 20,
-                  color: const Color(0xff000000),
-                ),
-                softWrap: false,
+            padding: const EdgeInsets.only(top: 220, left: 240),
+            child: Text(
+              'آخر المشاهدات',
+              style: TextStyle(
+                fontFamily: 'Tajawal',
+                fontSize: 20,
+                color: const Color(0xff000000),
               ),
+              softWrap: false,
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 360, top: 330),
+            padding: const EdgeInsets.only(left: 360, top: 224),
             child: Icon(Icons.restart_alt),
           ),
           Padding(
@@ -130,8 +137,41 @@ class _ProfileState extends State<Profile> {
             ),
           ),
           Padding(
+            padding: const EdgeInsets.only(left: 270, top: 300),
+            child: Text(
+              'الرسائل',
+              style: TextStyle(
+                fontFamily: 'Tajawal',
+                fontSize: 20,
+                color: const Color(0xff000000),
+                fontWeight: FontWeight.w500,
+              ),
+              softWrap: false,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 360, top: 300),
+            child: Icon(Icons.chat_outlined),
+          ),
+          Padding(
             padding: const EdgeInsets.only(left: 350, top: 160),
             child: Center(child: Icon(Icons.info_outline)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 260, top: 340),
+            child: Text(
+              'الاعدادات',
+              style: TextStyle(
+                fontFamily: 'Tajawal',
+                fontSize: 20,
+                color: const Color(0xff000000),
+                fontWeight: FontWeight.w500,
+              ),
+              softWrap: false,
+            ),
+          ),          Padding(
+            padding: const EdgeInsets.only(left: 360, top: 340),
+            child: Icon(Icons.settings),
           ),
           Padding(
             padding: const EdgeInsets.only(left: 190, top: 260),
